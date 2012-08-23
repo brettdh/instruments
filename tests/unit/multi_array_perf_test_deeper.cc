41ee6e9832977d50112ec99f2be520dce056d494
Brett Higgins <brettdh@eecs.umich.edu>
2012-08-23 16:52:14 -0400

    Testing to see if the multi-dimension array is a source of slowdown.  Answer: yep.

--- tests/unit/multi_array_perf_test_deeper.cc
+++ tests/unit/multi_array_perf_test_deeper.cc
@@ -7,22 +7,25 @@ using std::vector;
 
 int main()
 {
-    size_t size = 2;
-    const size_t rowlen = 5000;
+    size_t size = 4;
+    const size_t rowlen = 30;
     vector<size_t> dims;
     dims.resize(size, rowlen);
 
     vector<size_t> position;
     position.resize(size, 0);
     
-    double **raw_array = new double*[rowlen];
-    double *flat_array = new double[rowlen * rowlen];
+    double ****raw_array = new double***[rowlen];
     for (size_t i = 0; i < rowlen; ++i) {
-        raw_array[i] = new double[rowlen];
+        raw_array[i] = new double**[rowlen];
         for (size_t j = 0; j < rowlen; ++j) {
-            raw_array[i][j] = 42.0;
-
-            flat_array[i * rowlen + j] = 42.0;
+            raw_array[i][j] = new double*[rowlen];
+            for (size_t k = 0; k < rowlen; ++k) {
+                raw_array[i][j][k] = new double[rowlen];
+                for (size_t m = 0; m < rowlen; ++m) {
+                    raw_array[i][j][k][m] = 42.0;
+                }
+            }
         }
     }
     MultiDimensionArray<double> array(dims, 42.0);
@@ -34,7 +37,11 @@ int main()
     double sum = 0.0;
     for (size_t i = 0; i < rowlen; ++i) {
         for (size_t j = 0; j < rowlen; ++j) {
-            sum += raw_array[i][j];
+            for (size_t k = 0; k < rowlen; ++k) {
+                for (size_t m = 0; m < rowlen; ++m) {
+                    sum += raw_array[i][j][k][m];
+                }
+            }
         }
     }
     gettimeofday(&end, NULL);
@@ -48,26 +55,6 @@ int main()
 
     fprintf(stderr, "raw array took %f seconds; sum=%f\n", seconds, sum);
 
-    // ===== FLAT ARRAY ===== //
-
-    gettimeofday(&begin, NULL);
-    sum = 0.0;
-    for (size_t i = 0; i < rowlen; ++i) {
-        for (size_t j = 0; j < rowlen; ++j) {
-            sum += flat_array[i * rowlen + j];
-        }
-    }
-    gettimeofday(&end, NULL);
-    diff.tv_sec = end.tv_sec - begin.tv_sec;
-    if (end.tv_usec < begin.tv_usec) {
-        --diff.tv_sec;
-        end.tv_usec += 1000000;
-    }
-    diff.tv_usec = end.tv_usec - begin.tv_usec;
-    seconds = diff.tv_sec + (diff.tv_usec / 1000000.0);
-
-    fprintf(stderr, "flat array took %f seconds; sum=%f\n", seconds, sum);
-
     // ===== MultiDimensionArray ===== //
 
     gettimeofday(&begin, NULL);
@@ -76,7 +63,13 @@ int main()
         position[0] = i;
         for (size_t j = 0; j < rowlen; ++j) {
             position[1] = j;
-            sum += array.at(position);
+            for (size_t k = 0; k < rowlen; ++k) {
+                position[2] = k;
+                for (size_t m = 0; m < rowlen; ++m) {
+                    position[3] = m;
+                    sum += array.at(position);
+                }
+            }
         }
     }
     gettimeofday(&end, NULL);
