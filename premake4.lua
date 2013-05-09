b162c46f25751bab1faf0b6c2e24d21c8a74bef4
Brett Higgins <brettdh@eecs.umich.edu>
2013-05-09 09:51:41 -0400

    Added install premake target.

--- premake4.lua
+++ premake4.lua
@@ -7,3 +7,12 @@ solution "Instruments"
   include "src"
   include "tests/unit"
   include "tests/acceptance"
+
+  newaction {
+    trigger = "install",
+    description = "Install to /usr/local",
+    execute = function ()
+      os.execute("cp include/instruments.h /usr/local/include/")
+      os.execute("cp src/libinstruments.so /usr/local/lib/")
+    end
+  }
