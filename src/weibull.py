95ee135fefeac812db52067657c62a38b9e2b19c
Brett Higgins <brett.higgins@gmail.com>
2013-07-24 17:18:39 -0400

    Added settable weibull params, setting of params from CoNEXT paper, and plotting of failure probability.

--- src/weibull.py
+++ src/weibull.py
@@ -25,7 +25,18 @@ def cdf_xvalues(max_x):
     max_x *= 1.05
     return np.arange(0.0, max_x * 1.001, max_x * 0.001)
 
-def plot_fit(samples):
+def failure_prob(distribution, samples, failure_window):
+    '''Pr(X < upper | X >= lower)
+       = Pr(X < upper ^ X >= lower) / Pr(X >= lower)
+       = ( F(upper) - F(lower) ) / (1 - F(lower))
+    '''
+    cdf = distribution.cdf
+    lower = samples - failure_window
+    lower[lower < 0] = 0.0
+    upper = samples
+    return ( cdf(upper) - cdf(lower)) / (1.0 - cdf(lower))
+
+def plot_fit(samples, failure_window):
     shape, loc, scale = fit_weibull(samples)
     x = cdf_xvalues(max(samples))
 
@@ -36,14 +47,23 @@ def plot_fit(samples):
     num_samples = len(samples)
     cdf = np.arange(1, num_samples + 1) / float(num_samples)
     
-    plt.plot(sorted(samples), cdf, color="red", linestyle='none', marker='.', label='samples')
-    plt.plot(x, fit_cdf_values, color="blue", marker='none', label='fit')
+    plt.plot(sorted(samples), cdf, color="black", linestyle='none', marker='.', label='samples')
+    plt.plot(x, fit_cdf_values, color="blue", marker='none', label='fit CDF')
 
+    if failure_window:
+        failure_pdf = failure_prob(fit_dist, x, failure_window)
+        max_i, max_val = np.argmax(failure_pdf), np.max(failure_pdf)
+        print "Max failure prob %f at time %f" % (max_val, x[max_i])
+        plt.plot(x, failure_pdf, color="red", marker='none', 
+                 label="%d-second\nfailure PDF" % failure_window)
 
-def generate_weibull_samples(num_samples=100):
+def generate_weibull_samples(num_samples=100, params=None):
     fixed_shape = 2.0
     fixed_loc = 0.0
     fixed_scale = 4.0
+    if params:
+        fixed_shape, fixed_scale = params
+
     print "actual: (shape=%f loc=%f scale=%f)" % (fixed_shape, fixed_loc, fixed_scale)
     weibull = stats.weibull_min(fixed_shape, loc=fixed_loc, scale=fixed_scale)
 
@@ -53,27 +73,34 @@ def generate_weibull_samples(num_samples=100):
     cdf_values = weibull.cdf(x)
     samples = weibull.rvs(num_samples)
 
-    plt.plot(x, cdf_values, color="green", marker='none', label='actual')
+    plt.plot(x, cdf_values, color="green", marker='none', label='actual CDF')
     return samples
 
 def main():
-    #values = [int(x.strip()) for x in readlines(sys.stdin)]
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--gen-samples", type=int, metavar="NUM_SAMPLES")
+    parser.add_argument("--params", type=float, nargs=2, metavar=['SHAPE', 'SCALE'])
     parser.add_argument("--stdin", action="store_true", default=False)
+    parser.add_argument("--failure-window", type=int)
+    parser.add_argument("--kaist-paper-params", action="store_true", default=False)
     args = parser.parse_args()
     if args.gen_samples:
-        samples = generate_weibull_samples(args.gen_samples)
+        samples = generate_weibull_samples(args.gen_samples, args.params)
     elif args.stdin:
-        samples = [float(x.strip()) for x in readlines(sys.stdin)]
-    else:
-        samples = generate_weibull_samples()
+        samples = [float(x.strip()) for x in sys.stdin.readlines()]
+    elif args.kaist_paper_params:
+        samples = generate_weibull_samples(100000, params=[0.52, 6.17 * np.e - 4])
+        if args.failure_window:
+            args.failure_window /= 3600.0 # seconds to hours
+            print "Converted failure window to %f hours" % args.failure_window
         
-    plot_fit(samples)
+    else:
+        samples = generate_weibull_samples(params=args.params)
+        
+    plot_fit(samples, args.failure_window)
 
     plt.xlabel("value")
-    plt.ylabel("CDF")
     plt.legend(loc=4)
     plt.show()
 
